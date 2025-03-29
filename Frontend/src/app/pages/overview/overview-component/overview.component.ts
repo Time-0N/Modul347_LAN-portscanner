c61e85ecf8f4c0402eb330fe71d5e65ed9faf874
Timeon Haas <timeon.haas@gmail.com>
2025-03-29 21:16:58 +0100

    Added search on the network-list

--- Frontend/src/app/pages/overview/overview-component/overview.component.ts
+++ Frontend/src/app/pages/overview/overview-component/overview.component.ts
@@ -5,6 +5,9 @@ import {selectNetworks} from '../store/network-store/network.selectors';
 import {FormsModule} from '@angular/forms';
 import {NetworkListComponent} from '../network-list/network-list.component';
 import {AsyncPipe, NgIf} from '@angular/common';
+import {Network} from "../../../models/network";
+import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-overview',
@@ -19,14 +22,33 @@ import {AsyncPipe, NgIf} from '@angular/common';
   styleUrl: './overview.component.css'
 })
 export class OverviewComponent {
-
-  public readonly networks$;
   ipToScan: string = '';
+  searchTerm: string = '';
+
+  networks$: Observable<Network[]>;
+  filteredNetworks$: Observable<Network[]>;
 
   constructor(
     private readonly store: Store,
   ) {
-    this.networks$ = store.select(selectNetworks);
+    this.networks$ = this.store.select(selectNetworks);
+    this.filteredNetworks$ = this.networks$.pipe(
+        map(networks => this.filterNetworks(networks))
+    )
+  }
+
+  private filterNetworks(networks: Network[]): Network[] {
+    if (!this.searchTerm) return networks;
+    const term = this.searchTerm.toLowerCase();
+    return networks.filter(network =>
+        network.name?.toLowerCase().includes(term)
+    );
+  }
+
+  onSearchChange() {
+    this.filteredNetworks$ = this.networks$.pipe(
+        map(networks => this.filterNetworks(networks))
+    )
   }
 
   onScanSubmit() {
